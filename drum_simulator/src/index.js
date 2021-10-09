54ed008d024d8400fe9dcd6a42a80c2820037556
gtsimis <gt2186@gmail.com>
2021-10-09 13:58:59 -0400

    Added audio tags, set up src and data-keys and got sounds of drum to correspond to keydown event

---
+++ drum_simulator/src/index.js
@@ -0,0 +1,28 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const SOUNDS = [
+        ["KeyA", "COWBELL"],
+        ["KeyS", "HIHAT"],
+        ["KeyD", "KICK"],
+        ["KeyF", "OPENHIHAT"],
+        ["KeyJ", "SNARE"],
+        ["KeyK", "TOM"],
+        ["KeyL", "RIDE"],
+        ["Semicolon", "CRASH"],
+    ]
+
+    SOUNDS.forEach(pair => {
+        let audio = document.createElement("audio");
+        const audioDiv = document.getElementById("audio-tags");
+        audioDiv.append(audio);
+        let src = "./sounds/" + pair[1] + ".wav";
+        audio.src = src;
+        audio.dataset.key = `${pair[0]}`;
+    })
+    
+    window.addEventListener('keydown', function(e) {
+        const audio = document.querySelector(`audio[data-key="${e.code}"]`) 
+        audio.play(); 
+    }); 
+})
