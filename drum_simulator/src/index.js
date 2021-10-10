ff646086c2bb5cf32eb9847fe89e3d55879ac153
gtsimis <gt2186@gmail.com>
2021-10-10 12:43:06 -0400

    Dried up code for drumless track btns and audio tags and added eventt listeneers

--- drum_simulator/src/index.js
+++ drum_simulator/src/index.js
@@ -32,28 +32,65 @@ document.addEventListener("DOMContentLoaded", () => {
     function removeClass(e) {
         this.classList.remove('playing');
     }
-    
-    function playSound(e) {
-        const drumless = document.getElementById("get-lit");
-        drumless.currentTime = 0;
-        drumless.play();
-    }
-
-    function playMusic(e) {
-        const drumless = document.getElementById("get-crazy");
-        drumless.currentTime = 0;
-        drumless.play();
-    }
 
     const keys = document.querySelectorAll(".key")
-    //console.log(keys)
+
     // adding an event listener to each key to remove class of playing after transition ends
     keys.forEach(key => key.addEventListener('transitionend', removeClass));
+    
+    //
+    // ** drumless tracks section **
+    //
+    
+    const TRACKS = [
+        ['btn1', 'track1', 'Track 1'],
+        ['btn2', 'track2', 'Track 2'],
+        ['btn3', 'track3', 'Track 3']
+    ];
 
-    const btn = document.getElementById('btn')
-    btn.addEventListener('click', playSound)
+    TRACKS.forEach(track => {
+        // creating buttons with attributes and appending to correcr HTML ele
+        const trackNav = document.querySelector(".drumless");
+        let button = document.createElement("button")
+        button.innerText = track[2]
+        button.id = track[1];
+        button.classList.add('btn');
+        button.dataset.song = track[1];
+        trackNav.append(button);
+        // creating audio tags with attributes and appending to correcr HTML ele
+        const audioDiv = document.getElementById("audio-tags");
+        let audio = document.createElement("audio")
+        let src = "./sounds/" + track[1] + ".wav";
+        audio.src = src;
+        audio.dataset.song = track[1]; 
+        audioDiv.append(audio);
+    })
 
-    const btn2 = document.getElementById('btn2')
-    btn2.addEventListener('click', playMusic)
+    function playSound(e) {
+        const track = document.querySelector(`audio[data-song="${e.target.id}"]`);
+        // drumless.currentTime = 0;
+        track.play();
+        //console.log(e.target.id);
+    }
+    
+    const btns = document.querySelectorAll(".btn");
+    btns.forEach(btn => {
+        btn.addEventListener('click', playSound)
+    })
+
+    // function playMusic(e) {
+    //     const drumless = document.getElementById("get-crazy");
+    //     drumless.currentTime = 0;
+    //     drumless.play();
+    // }
+
+    // const btn = document.getElementById('btn')
+    // const btn2 = document.getElementById('btn2')
+    // btn2.addEventListener('click', function(e){
+    //     console.log(e);
+    // })
+
+    // const btn2 = document.getElementById('btn2')
+    // btn2.addEventListener('click', playMusic)
     
 })
